7d05b847e9a9f839ff824afe995d5991e4474cbf
wayTo <yurigarciadev@gmail.com>
2024-02-05 17:18:57 -0300

    https

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
+import cors from "cors";
 import express from "express";
+import fs from "fs";
+import https from "https";
 import multer from "multer";
 import path from "path";
-const app = express();
 const port = 8082;
+
+process.on("uncaughtException", (err) => {
+  console.error("\x1b[31m%s\x1b[0m", "UNCAUGHT EXCEPTION!");
+  console.error("\x1b[31m%s\x1b[0m", err);
+});
+
+const app = express();
+
+const allowedOrigins = ["http://localhost:3000", "https://javelyn.vercel.app"]; // Substitua pelos seus valores
+
+const corsOptions: cors.CorsOptions = {
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE", "PROPFIND"],
+};
+
+app.use(cors(corsOptions));
+
+app.use(multer().single("file"));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 // Configurar o multer para o upload de arquivos
 const storage = multer.diskStorage({
@@ -30,6 +52,19 @@ app.post("/upload", upload.single("image"), (req, res) => {
   res.json({ message: "Upload realizado com sucesso.", filePath: filePath });
 });
 
-app.listen(port, () => {
-  console.log(`FTP-Uploader server running on port: ${port}`);
-});
+const options = {
+  key: fs.readFileSync(
+    "../../../etc/letsencrypt/live/javelyn.link/privkey.pem"
+  ),
+  cert: fs.readFileSync(
+    "../../../etc/letsencrypt/live/javelyn.link/fullchain.pem"
+  ),
+};
+const server = https
+  .createServer(options, app)
+  .listen(port, () =>
+    console.log(
+      `ftp-uploader server online on port ${port} and using node version ` +
+        process.version
+    )
+  );
